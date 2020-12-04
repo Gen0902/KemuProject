62c50a2c11af3e52cddb4ac7b9c9021416948001
MattOuvrard <50735058+MattOuvrard@users.noreply.github.com>
2020-12-04 15:36:51 +0100

    projet castle crusher

---
+++ Castle_crusher/Assets/Materials/VFX_material/Shader/DissolveShader.shader
@@ -0,0 +1,72 @@
+﻿Shader "Custom/DissolveShader"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _NoiseTex("Noise Texture", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+        _DissolveAmount ("Dissolve Amount", Range(0,1)) = 1
+        _EmiColor("Color Emission", Color) = (1,1,1)
+        _EmiAmount("Amount Border", Range(0,1)) = 0.5
+        //StandardProperties
+        _BumpMap ("Normal Map", 2D) ="white" {}
+        _EmissionColor("Emission Color", Color) = (1,1,1,1)
+        _EmissionMap("Emission Map", 2D) = "white"{}
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+        sampler2D _NoiseTex;
+        half _DissolveAmount;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        fixed3 _EmiColor;
+        half _EmiAmount;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            half dissolveValue = tex2D(_NoiseTex, IN.uv_MainTex).r;
+            clip(dissolveValue- _DissolveAmount);
+
+
+            // Albedo comes from a texture tinted by color
+            fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+            o.Albedo = c.rgb;
+
+            o.Emission = _EmiColor * step(dissolveValue - _DissolveAmount, _EmiAmount);
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Castle_crusher/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            instance = _instance;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    int level;
+    public int levelMax;
+    int nbTotemTomber;
+    int nbTotem;
+    GameObject LevelActuel;
+    bool Brouillard;
+    [SerializeField]
+    Text_Manager m_text; 
+    [SerializeField]
+    Transform centerLevel;
+    public float vitesseBrouillard;
+    public GameObject Halo;
+    public BallManager m_ballManag;
+    public GameObject Cache;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nbTotemTomber = 0;
+        level = -1;
+        StartCoroutine("ChangerLevel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Brouillard)
+        {
+            if( RenderSettings.fogEndDistance > 20)
+            {
+
+                // reduit le brouillard
+                RenderSettings.fogEndDistance -= vitesseBrouillard;
+            }
+            else
+            {
+                Cache.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+        else if (!Brouillard && RenderSettings.fogEndDistance < 200)
+        {
+
+            //on augmente le brouillard
+            RenderSettings.fogEndDistance += vitesseBrouillard *2;
+            if(RenderSettings.fogEndDistance > 185)
+            {
+                RenderSettings.skybox = Resources.Load<Material>("SkyBox/HDRMarine");
+                changerNombreAAbattre();
+                Cache.GetComponent<MeshRenderer>().enabled = false;
+            }
+                
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //level++;
+            //StartCoroutine("ChangerLevel");
+        }
+    }
+
+    IEnumerator ChangerLevel()
+    {
+        nbTotemTomber = 0;
+        nbTotem = 0;
+        level++;
+
+        if(level == levelMax)
+        {
+            m_text.ChangeText("You Win");
+        }
+
+        // ici on fait apparaitre le brouillard
+        Brouillard = true;
+        RenderSettings.skybox = Resources.Load<Material>("SkyBox/HDRMatt");
+        m_text.ChangeText("Wait");
+        yield return new WaitForSeconds(3);
+
+        // on supprime l'ancien level
+        Destroy(LevelActuel);
+        LevelActuel = null;
+        m_ballManag.CleanBall();
+
+        // puis on charge nouveau level
+        var temp = Instantiate(Resources.Load<GameObject>("Level/niv_" + level + "/Level" + level + Random.Range(0, 2)), centerLevel);
+        LevelActuel = temp; //Resources.Load<GameObject>("Level/niv_" + level + "Level"+ level + Random.Range(0, 3));
+        if (LevelActuel != null)
+        {
+            foreach (Transform t in LevelActuel.transform)// on defini le nombre de totem a faire tomber
+            {
+                if (t.gameObject.tag == "Totem")
+                {
+                    nbTotem++;
+                }
+            }
+            if (LevelActuel.GetComponent<LevelManager>())
+            {
+                m_ballManag.CreateBall(LevelActuel.GetComponent<LevelManager>().config);
+            }
+        }
+
+        yield return new WaitForSeconds(1);
+
+        // puis on change a nouveau la brume
+        Brouillard = false;
+    }
+
+    public void EstCequeLeDernierTotemEstTomber()
+    {
+       
+            if(nbTotemTomber < nbTotem)
+            {
+                changerNombreAAbattre();
+                return; // y en a toujours qui son en lice
+            }
+
+        StartCoroutine("ChangerLevel");
+    }
+
+    public void TotemTomber(Vector3 pos)
+    {
+        nbTotemTomber++;        
+        Instantiate(Halo, new Vector3(pos.x, Halo.transform.position.y, pos.z), Quaternion.identity, m_ballManag.transform);
+        EstCequeLeDernierTotemEstTomber();
+    }
+
+
+    public void changerNombreAAbattre()
+    {
+        if(nbTotem - nbTotemTomber == 1)
+        {
+            m_text.ChangeText("Shoot It");
+        }
+        else
+        {
+            int t = nbTotem - nbTotemTomber;
+            m_text.ChangeText("Shoot " + t);
+        }
+
+    }
+
+}
